99c1a76924a1f62d6983af82e95e7b2927a1d2f1
hinkley-dev <hinkley.dev@gmail.com>
2024-05-30 12:17:15 -0600

    cleaning up for sandbox usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
+import 'ag-grid-enterprise'
 import { Component } from '@angular/core';
+import { GridOptions } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <ag-grid-angular
+      style="width: 100%; height: 500px;"
+      class="ag-theme-alpine"
+      [rowData]="gridOptions.rowData"
+      [columnDefs]="gridOptions.columnDefs">
+    </ag-grid-angular>
+  `,
 })
 export class AppComponent {
   title = 'ag-grid-sandbox';
+
+   gridOptions: GridOptions<any> = {
+
+    columnDefs :[
+      { headerName: 'Make', field: 'make' },
+      { headerName: 'Model', field: 'model' },
+      { headerName: 'Price', field: 'price' }
+    ],
+
+    rowData : [
+      { make: 'Toyota', model: 'Celica', price: 35000 },
+      { make: 'Ford', model: 'Mondeo', price: 32000 },
+      { make: 'Porsche', model: 'Boxster', price: 72000 }
+    ]
+  }
 }
